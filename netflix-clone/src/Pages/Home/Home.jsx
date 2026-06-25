4afbd5a641eb3bba835b44b80751239f91ca0a0a
Milion Sagni <milionsagni1995@gmail.com>
2026-06-25 09:12:44 +0300

    Movie Rows

--- netflix-clone/src/Pages/Home/Home.jsx
+++ netflix-clone/src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/Banners/Banner.jsx'
+import RowList from '../../components/RowsList/RowList'
 
 const Home = () => {
   return (
     <>
     <Header/>
     <Banner/>
+    <RowList/>
     <Footer/>
     
     </>
